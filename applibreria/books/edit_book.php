f66aae126fcb990325f64f92be4042fd308b29c6
Giuseppe Sansone <giuseppesansone144@gmail.com>
2024-02-07 18:27:41 +0100

    done

--- applibreria/books/edit_book.php
+++ applibreria/books/edit_book.php
@@ -56,7 +56,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
     <title>Modifica Libro</title>
 </head>
-<body>
+<body> 
     
     <h1 class="mb-4">Modifica Libro</h1>
     <button class="btn btn-primary" style='width: 18rem;' >
